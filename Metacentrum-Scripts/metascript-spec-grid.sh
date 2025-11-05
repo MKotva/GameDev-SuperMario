ca766f03ce2c05e27584f875454a5eedf096d66f
Milan Kotva <mkotva@tarkil.metacentrum.cz>
2025-11-06 00:25:43 +0100

    Results added

--- Metacentrum-Scripts/metascript-spec-grid.sh
+++ Metacentrum-Scripts/metascript-spec-grid.sh
@@ -2,11 +2,11 @@
 
 # seq -> FIRST INCREMENT LAST
 
-for ssc in 1 2 3 4 5 6 7 8 9 10 20
+for ssc in 1 3 4 5 6 7 8 9 10 20
 do
   for ttfw in 0.20 0.40 0.60 0.80 1.00 1.20 1.40 1.60 1.80 2.00
   do
         echo $ndw $ttfw $dfpt $dfpap
-	qsub -v NDW=$ndw,TTFW=$ttfw,DFPT=$dfpt,DFPAP=$dfpap script-spec-grid.sh
+	qsub -v SSC=$ssc,TTFW=$ttfw script-spec-grid.sh
   done
 done
